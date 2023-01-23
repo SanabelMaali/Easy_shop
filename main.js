3b62b9a69854db5da6a568fb49248701cc1e408d
OsaidAttar <osedatar2017@gmail.com>
2023-01-23 20:28:11 +0200

    navbar and footer

--- main.js
+++ main.js
@@ -2,6 +2,19 @@ var page2=document.getElementById('click') ;
 var page3=document.getElementById('click') ;
 var prod9=document.getElementById('prod9')
 
+let navbar =document.getElementById('navbar')
+window.addEventListener("scroll",function(){
+    if(window.scrollY > 50){
+       navbar.classList.add('new-bg-color')
+       navbar.style.transition='1s'
+    }
+    else{
+        navbar.classList.remove('new-bg-color')
+        navbar.style.transition='1s'
+
+    }
+    
+})
 page2.onclick=function(event){
 
     var products={
